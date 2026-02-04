d86b22177642458c3fddbd1667ea68c3d35d5aeb
fenris <fenris06@rambler.ru>
2026-02-04 22:29:06 +0300

    rsocket request-response, request-stream

---
+++ rsocket/request-stream/server/src/main/java/rsocket/RSocketRequestStreamApplication.java
@@ -0,0 +1,11 @@
+package rsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RSocketRequestStreamApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RSocketRequestStreamApplication.class, args);
+    }
+}
